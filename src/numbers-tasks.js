6ef85009a027e697320803eb8e23b240efcb8cf0
Aleksandr <Nadtochaev_aleksandr@mail.ru>
2024-12-07 16:28:29 +0300

    fix: did 12 task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -241,8 +241,17 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  let j = 0;
+  for (let i = 1; i <= n ** 0.5; i += 1) {
+    if (n % i === 0) {
+      j += 1;
+    }
+  }
+  if (j > 1) {
+    return false;
+  }
+  return true;
 }
 
 /**
